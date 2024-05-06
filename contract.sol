358c4e05a7a57979d909086698c2b9e729f87278
Marc <101866347+Web3Paradise@users.noreply.github.com>
2024-05-06 13:53:39 +0100

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,1 +1,21 @@
+pragma solidity ^0.8.0;
 
+contract SampleContract {
+    address public owner;
+    uint public balance;
+
+    constructor() public {
+        owner = msg.sender;
+        balance = 0;
+    }
+
+    function increaseBalance() public {
+        balance += 1;
+    }
+
+    function transfer(address _to) public {
+        if (msg.sender == owner) {
+            _to.transfer(balance);
+        }
+    }
+}
